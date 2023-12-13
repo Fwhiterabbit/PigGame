bcc1a15564dabe84cd6c8cdc522e7b6a6e078169
Fwhiterabbit <thisismichael91@gmail.com>
2023-12-13 21:46:15 +0000

    added player switch

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 // selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const diceEl = document.querySelector('.dice');
 const current1El = document.getElementById('current--1');
 const current0El = document.getElementById('current--0');
 
+const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
@@ -14,7 +16,9 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+const scores =[0, 0];
 let currentScore = 0;
+let activePlater = 0;
 
 // rolling dice functionality
 btnRoll.addEventListener('click', function () {
@@ -30,7 +34,11 @@ btnRoll.addEventListener('click', function () {
     if(dice !== 1) {
         // Add dice to current score
         currentScore += dice;
+        document.getElementById(`current--${activePlater}`).textContent = currentScore;
     } else {
-        
+        // Switch to next player
+        document.getElementById(`current--${activePlater}`).textContent = 0;
+        currentScore = 0;
+        activePlater = activePlater === 0 ? 1 : 0;
     }
 });
